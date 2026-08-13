59bf513942468bf42e44eac46d53f8aac3b8750e
Yelyzaveta Kravchuk <lizacherchenkov@gmail.com>
2026-08-13 23:01:24 +0200

    Resolve merge conflict in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   allowCypressEnv: true,
@@ -16,28 +16,18 @@ module.exports = defineConfig({
 
   e2e: {
     specPattern: "cypress/e2e/features/**/*.feature",
-    baseUrl: 'https://practicesoftwaretesting.com',
+    baseUrl: 'https://practiceofswtesting.com',
 
     setupNodeEvents: async (on, config) => {
       // Cucumber plugin with tag support
-      const tags = config.env.tags;
-      await addCucumberPreprocessorPlugin(on, config, {
-        omitFiltered: true,
-        filterSpecs: true,
-      });
+      await addCucumberPreprocessorPlugin(on, config);
 
-      // Esbuild bundler for Cucumber
       on("file:preprocessor", createBundler({
         plugins: [createEsbuildPlugin(config)],
       }));
 
       // Mochawesome reporter
       require('cypress-mochawesome-reporter/plugin')(on);
-
-      // Support tag filtering through environment variable
-      if (tags) {
-        config.env.TAGS = tags;
-      }
 
       return config;
     },
